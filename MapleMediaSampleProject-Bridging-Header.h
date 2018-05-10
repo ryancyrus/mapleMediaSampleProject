c377ff2d2408b9c9b4bf367dcc598f320335396e
Ryan Cyrus <ryan.cyrus@live.com>
2018-05-10 15:34:16 +0530

    Adding the missing Bridging Header which was left out in previous commit. This should prevent any errors which appeared.

---
+++ MapleMediaSampleProject-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  MapleMediaSampleProject-Bridging-Header.h
+//  MapleMediaSampleProject
+//
+//  Created by Ryan on 2018-05-10.
+//  Copyright © 2018 Ryan. All rights reserved.
+//
+
+#ifndef MapleMediaSampleProject_Bridging_Header_h
+#define MapleMediaSampleProject_Bridging_Header_h
+
+
+#endif /* MapleMediaSampleProject_Bridging_Header_h */
+
+
+#import "MoPubSDK/MoPub-Bridging-Header.h"
+
