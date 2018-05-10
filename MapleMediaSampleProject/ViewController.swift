21b05ca5b65fb5a738c789e7d7e55401373d2f55
Ryan Cyrus <ryan.cyrus@live.com>
2018-05-10 08:28:26 +0530

    Initial Commit

---
+++ MapleMediaSampleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapleMediaSampleProject
+//
+//  Created by Ryan on 2018-05-10.
+//  Copyright © 2018 Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
